f876b6477608fbde1f0d3395efe91ede02bacc41
Marcelo Marchetto <marcelomarchetto@MacBook-Pro-de-Marcelo.local>
2022-01-27 11:22:01 -0300

    bug fixes 4.0

--- pages/posts/[titulo].tsx
+++ pages/posts/[titulo].tsx
@@ -12,10 +12,9 @@ interface Post{
 
 export const getStaticPaths = async () => {
     
-    const requestDada = await axios.get(`${process.env.FETCHING_URL}/post`)
-    const createPaths:any[] = requestDada.data
+    const {data} = await axios.get(`${process.env.FETCHING_URL}/post`)
     
-    const paths = createPaths.map((post:Post) => {
+    const paths = data.map((post:Post) => {
       return { params: { titulo: post.titulo } };
     });
   
